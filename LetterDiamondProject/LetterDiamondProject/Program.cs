df526b31a0ad9bdfc4d7264226c9262e3ecb610f
prmalone2 <prmalone2@wpi.edu>
2018-10-09 13:49:11 -0400

    added comments (whoops)

--- LetterDiamondProject/LetterDiamondProject/Program.cs
+++ LetterDiamondProject/LetterDiamondProject/Program.cs
@@ -8,11 +8,13 @@ namespace LetterDiamondProject
 {
     class Program
     {
+        //returns next letter in alphabet
         public static char nextLetter(char letter)
         {
             return (char)((int)letter + 1);
         }
 
+        //takes input letter and creates array in abc order
         public static List<char> characterArray(char lastLetter)
         {
             List<char> charList = new List<char>();
@@ -26,6 +28,7 @@ namespace LetterDiamondProject
             return characterArrayPt2(charList);
         }
 
+        //adds 'reverse-side' of array to make printing easier
         public static List<char> characterArrayPt2(List<char> list)
         {
             int size = list.Count;
@@ -36,14 +39,16 @@ namespace LetterDiamondProject
             return list;
         }
 
+        //takes array of letters and formats it into diamond form
         public static void printDiamond(List<char> list)
         {
-            int numberOfInternalSpaces;
-            int numberOfExternalSpaces;
-            int maxExternalSpaces = (list.Count - 1) / 2;
+            int numberOfInternalSpaces; //spaces between letters
+            int numberOfExternalSpaces; //spaces before first letter on line
+            int maxExternalSpaces = (list.Count - 1) / 2; //for first and last line
             for (int i = 0; i < list.Count; i++)
             {
-                numberOfExternalSpaces = Math.Abs(i - maxExternalSpaces);
+                numberOfExternalSpaces = Math.Abs(i - maxExternalSpaces); //calculate external spaces
+                //calculate internal spaces
                 numberOfInternalSpaces = (maxExternalSpaces - numberOfExternalSpaces) * 2 - 1;
                 if (i == 0 || i == (list.Count - 1))
                 {
@@ -73,6 +78,10 @@ namespace LetterDiamondProject
             }
         }
 
+        //if input is not a letter or is multiple characters long, will print message and ask
+        //user to input new character
+        //user escapes app by typing in "escape"
+        //letters can be in any case for escape functionality to work
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a letter to form a diamond, or type 'escape' to exit");
